9f570121e720f6a1134fcf27c54f1b11ff3b445a
haridhayal11 <haridhayal@gmail.com>
2021-01-07 22:11:17 +0530

    Sakurafied

--- lineage_m20lte.mk
+++ lineage_m20lte.mk
@@ -4,3 +4,6 @@ $(call inherit-product, device/samsung/m20lte/full_m20lte.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_m20lte
+SAKURA_MAINTAINER := Haridhayal
+TARGET_BOOT_ANIMATION_RES := 1080
+SAKURA_OFFICIAL := true
